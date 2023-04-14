467bf4194b2d81b54bf61575b5d11fd6d244a096
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2023-04-14 20:31:59 +0200

    Update ControllerUnit.vhd

--- ControllerUnit.vhd
+++ ControllerUnit.vhd
@@ -6,16 +6,9 @@ use IEEE.numeric_std.all;
 entity ControlUnit is
   port (
   opCode,Func: in std_logic_vector(2 downto 0);
-  MemWrite: out std_logic;
-  MemRead: out std_logic;
-  RegWrite: out std_logic;
-  FlagSelcetor: out std_logic;
-  Branch: out std_logic;
-  InEnb: out std_logic;
-  OutEnb: out std_logic;
-  AddressSelector: out std_logic;
+  
   AluSelector: out std_logic_vector(2 downto 0);
-  UncondBranch: out std_logic
+  ContrullerSignel: out std_logic_vector(8 downto 0);
   ) ;
 end ControlUnit;
 
@@ -34,14 +27,15 @@ Else Not Func(0)& Func(0)& Func(0)&"000010" When opCode = "110"
 Else Not Func(0)& Func(0)&"0010011" When opCode = "111"
 Else "000000000";
 
-MemWrite<=temp(8);
-MemRead<=temp(7);
-RegWrite<=temp(6);
-FlagSelcetor<=temp(5);
-Branch<=temp(4);
-InEnb<=temp(3);
-OutEnb<=temp(2);
-AddressSelector<=temp(1);
-UncondBranch<=temp(0);
+-- MemWrite<=temp(8);
+-- MemRead<=temp(7);
+-- RegWrite<=temp(6);
+-- FlagSelcetor<=temp(5);
+-- Branch<=temp(4);
+-- InEnb<=temp(3);
+-- OutEnb<=temp(2);
+-- AddressSelector<=temp(1);
+-- UncondBranch<=temp(0);
+ContrullerSignel<=temp;
 AluSelector<=Func;
 end archOfConTrolUnit ; -- archOfConTrolUnit
