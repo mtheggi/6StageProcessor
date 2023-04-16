63f4136ea3a91d59ac41a7503d4c601028106b82
mtheggi <mtheggi@gmail.com>
2023-04-16 17:34:01 +0200

    SP Clk add Datamem Latch

--- SP.vhd
+++ SP.vhd
@@ -3,7 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all; 
 
 entity SP is
-    Port ( mem_read : in STD_LOGIC;
+    Port ( 
+            mem_read ,Clk : in STD_LOGIC;
            address_select : in STD_LOGIC;
            data_out : out STD_LOGIC_VECTOR (9 downto 0)
            );
@@ -12,9 +13,10 @@ end SP;
 architecture Behavioral of SP is
     signal stack_pointer : unsigned(9 downto 0) := "1111111110"; -- 1022 in decimal
 begin
-    process (mem_read, address_select)
+    process (Clk , mem_read, address_select)
 	variable temp_stack_pointer : unsigned(9 downto 0);
     begin
+        if Clk = '1' then 
         if address_select = '1' then
             if mem_read = '1' then -- pop 
                 temp_stack_pointer := stack_pointer +1 ;   
@@ -27,6 +29,7 @@ begin
                 stack_pointer <= temp_stack_pointer;
             end if;
         end if;
+        end if; 
     end process; 
  
  end Behavioral;
