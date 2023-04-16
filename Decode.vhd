9b251098d73ad3891712164059f41e081ca0a7c2
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2023-04-16 20:36:20 +0200

    Update Decode.vhd

--- Decode.vhd
+++ Decode.vhd
@@ -2,21 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity integration is
-  port (
-    clk, rst, int: in std_logic
-  ) ;
-
-end integration;
-
-architecture archinteg of integration is
-component fetch is
-port( rst, clk, branch:in std_logic;
-	branch_update: in std_logic_vector ( 15 downto 0);
-	updated_PC: out std_logic_vector ( 15 downto 0);
-      inst: out std_logic_vector(31 downto 0));
-end component;
-component Decode is
+entity Decode is
   port (
     clk, rst, WriteEnable: in std_logic;
     inst: in std_logic_vector(31 downto 0);
@@ -27,37 +13,75 @@ component Decode is
     WriteData: in std_logic_vector(15 downto 0);
     immediateVal, ReadPort1, ReadPort2: out std_logic_vector(15 downto 0)
   ) ;
+
+end Decode;
+
+architecture archOfDecode of Decode is
+component ControlUnit is
+  port (
+  opCode,Func: in std_logic_vector(2 downto 0);
+  AluSelector: out std_logic_vector(2 downto 0);
+  ControllerSignal: out std_logic_vector(9 downto 0)
+  ) ;
 end component;
-component IntMux is
+component RegistersFile is
+    generic (n: integer:= 16);
     port (
-      intOp:in std_logic;
-      OpcodePlusFunc: in std_logic_vector(5 downto 0); 
-    Rs,PC:in std_logic_vector(15 downto 0);
-    OutofMux: out std_logic_vector(15 downto 0)
-    ) ;
-    end component;
-component Reg is
-    generic(n: integer:=16);
-      port(
-        
-        d: in std_logic_vector(n-1 downto 0);
-        clk, rst, enable: in std_logic;
-        q: out std_logic_vector(n-1 downto 0)
-      );
+        WritePort: in std_logic_vector(n - 1 DOWNTO 0);
+	      ReadPort1,ReadPort2: out std_logic_vector(n - 1 DOWNTO 0);
+        WriteAddress, ReadAddress1,ReadAddress2: in std_logic_vector(2 DOWNTO 0);
+        CLK, RST, WriteEnable:  in std_logic
+    );
 end component;
-signal AluSelector, rs, rt, rd: std_logic_vector(2 downto 0);
-signal rs_data, rt_data: std_logic_vector(15 downto 0);
-signal FD_out, FD_in: std_logic_vector(47 downto 0);
-signal instruction: std_logic_vector(31 downto 0);
-signal immediateVal, updated_PC,ResofMux: std_logic_vector(15 downto 0);
-signal identifierBit:  std_logic;
-signal ControllerSignal: std_logic_vector (9 downto 0);
-signal OpcodePlusFunc: std_logic_vector (5 downto 0);
+signal opcode: std_logic_vector(2 downto 0);
+signal func: std_logic_vector(2 downto 0);
+signal rs_sig: std_logic_vector(2 downto 0);
+signal rt_sig: std_logic_vector(2 downto 0);
+signal rd_sig: std_logic_vector(2 downto 0);
+signal imm_sig: std_logic_vector(15 downto 0);
 begin
-FD_in <= updated_PC & instruction;
-OpcodePlusFunc<=instruction(31 downto 29)&AluSelector;
-f: fetch port map (rst, clk, ControllerSignal(4), rs_data,  updated_PC, instruction);
-FD: Reg generic map(48) port map (FD_in, clk, rst, '1', FD_out);
-d: Decode port map (clk, rst, '0', FD_out(31 downto 0), ControllerSignal, identifierBit, AluSelector, rs, rt, rd, "000", (others => '0'), immediateVal, rs_data, rt_data);--Write en, address, data from WB
-MuxBetWeenIntAndPush: IntMux port map (int,OpcodePlusFunc,rs_data,FD_out(47 downto 32),ResofMux);
-end archinteg;
+    opcode <= inst(31 downto 29);
+    func <= inst(28 downto 26);
+    rs_sig <= inst(24 downto 22);
+    rt_sig <= inst(21 downto 19);
+    rd_sig <= inst(18 downto 16);
+    imm_sig <= inst(15 downto 0);
+    identifierBit<=inst(25);
+    rs<=rs_sig;
+    rt<=rt_sig;
+    rd<=rd_sig;
+    immediateVal<=imm_sig;
+
+    CU: ControlUnit port map(opcode, func, AluSelector, ControllerSignal);
+
+    RF: RegistersFile port map(WriteData, ReadPort1, ReadPort2, WriteAddress, rs_sig, rt_sig, clk, rst, WriteEnable);
+end archOfDecode ; -- archOfDecode
+
+
+-- 3-bit opcode 
+
+-- (inst[31-29]) 
+
+-- 3-bit Funct 
+
+-- (inst[28-26]) 
+
+-- 16/32 identifier bit 
+
+-- (inst[25]) 
+
+-- 3-bit Rs 
+
+-- (inst[24-22]) 
+
+-- 3-bit rt 
+
+-- (inst[21-19]) 
+
+-- 3-bit rd 
+
+-- (inst[18-16]) 
+
+-- 16-bit immediate value 
+
+-- (inst[15-0]) 
