a4e2394989911fcaf13a925da086540a256b79e6
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2023-04-16 20:36:42 +0200

    Update Integration.vhd

--- Integration.vhd
+++ Integration.vhd
@@ -28,6 +28,14 @@ component Decode is
     immediateVal, ReadPort1, ReadPort2: out std_logic_vector(15 downto 0)
   ) ;
 end component;
+component IntMux is
+    port (
+      intOp:in std_logic;
+      OpcodePlusFunc: in std_logic_vector(5 downto 0); 
+    Rs,PC:in std_logic_vector(15 downto 0);
+    OutofMux: out std_logic_vector(15 downto 0)
+    ) ;
+    end component;
 component Reg is
     generic(n: integer:=16);
       port(
@@ -41,12 +49,15 @@ signal AluSelector, rs, rt, rd: std_logic_vector(2 downto 0);
 signal rs_data, rt_data: std_logic_vector(15 downto 0);
 signal FD_out, FD_in: std_logic_vector(47 downto 0);
 signal instruction: std_logic_vector(31 downto 0);
-signal immediateVal, updated_PC: std_logic_vector(15 downto 0);
+signal immediateVal, updated_PC,ResofMux: std_logic_vector(15 downto 0);
 signal identifierBit:  std_logic;
 signal ControllerSignal: std_logic_vector (9 downto 0);
+signal OpcodePlusFunc: std_logic_vector (5 downto 0);
 begin
 FD_in <= updated_PC & instruction;
+OpcodePlusFunc<=instruction(31 downto 29)&AluSelector;
 f: fetch port map (rst, clk, ControllerSignal(4), rs_data,  updated_PC, instruction);
 FD: Reg generic map(48) port map (FD_in, clk, rst, '1', FD_out);
 d: Decode port map (clk, rst, '0', FD_out(31 downto 0), ControllerSignal, identifierBit, AluSelector, rs, rt, rd, "000", (others => '0'), immediateVal, rs_data, rt_data);--Write en, address, data from WB
+MuxBetWeenIntAndPush: IntMux port map (int,OpcodePlusFunc,rs_data,FD_out(47 downto 32),ResofMux);
 end archinteg;
