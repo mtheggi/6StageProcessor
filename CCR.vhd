79771e07b38fa45409b0633fede01327d42823ad
mtheggi <mtheggi@gmail.com>
2023-04-17 18:02:37 +0200

    old CCR back

--- CCR.vhd
+++ CCR.vhd
@@ -4,28 +4,18 @@ use IEEE.numeric_std.all;
 
 entity CCR is
     port (
-        CCRDatain: IN std_logic_vector(2 downto 0);
-	CCRDataout: out std_logic_vector(2 downto 0);
-        FlagSelector, clk, rst: in std_logic;
+        CCRData: IN std_logic_vector(2 downto 0);
+        FlagSelcetor: in std_logic;
         FlagOutput: out std_logic
     ); -- 2: Carry, 1: Zero, 0: negative
 end entity CCR;
 
 architecture ControlRegister of CCR is
-    component Reg is
-    generic(n: integer:=16);
-      port(
-        
-        d: in std_logic_vector(n-1 downto 0);
-        clk, rst, enable: in std_logic;
-        q: out std_logic_vector(n-1 downto 0)
-      );
-end component;
-signal Current_flags: std_logic_vector(2 downto 0);
+    
 begin
-    with FlagSelector select
-        FlagOutput <= Current_flags(2) when '0',
-                      Current_flags(1) when others;
-flags: reg generic map (3) port map(CCRDatain, clk, rst, '1', Current_flags);
-CCRDataout <= current_flags;
+    with FlagSelcetor select
+        FlagOutput <= CCRData(2) when '0',
+                      CCRData(1) when others;
+    
+    
 end architecture ControlRegister;
