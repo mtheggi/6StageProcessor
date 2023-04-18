ffefa6f351e7fda6b9841a738b6402d174aabb0d
Mina <mina.fawzy02@eng-st.cu.edu.eg>
2023-04-18 18:05:02 +0200

    Execute has RST

--- Execute.vhd
+++ Execute.vhd
@@ -7,7 +7,7 @@ entity Execute is
         ControlSignals : IN std_logic_vector(9 downto 0);
         Rs, Rt, Immediate: IN std_logic_vector(15 downto 0);
         ALUFunction: IN std_logic_vector(2 downto 0);
-        identifierBit: IN std_logic;
+        identifierBit, RST: IN std_logic;
         OutputPort, ALUResult: OUT std_logic_vector(15 downto 0);
         ControllerSignalOut: OUT std_logic_vector(5 downto 0);
         CCROut: out std_logic_vector(2 downto 0); -- For RTI
@@ -36,14 +36,15 @@ architecture Exec of Execute is
     end component;
 
     signal ALUB: std_logic_vector(15 downto 0);
-    signal ALUFlags, OutputBeforeMux: std_logic_vector(2 downto 0);
+    signal ALUFlags, ALUFlagsTemp, OutputBeforeMux: std_logic_vector(2 downto 0);
     signal STCorCLC: std_logic;
     signal MuxSelector: std_logic_vector(1 downto 0);
+    signal currentRTIBit: std_logic;
     
 begin
-    ControllerSignalOut <= ControlSignals(8 downto 6) & ControlSignals(4) & ControlSignals(3) & ControlSignals(1);
+    ControllerSignalOut <= ControlSignals(8 downto 6) & currentRTIBit & ControlSignals(3) & ControlSignals(1);
     CCROut <= ALUFlags;
-
+    currentRTIBit <= ControlSignals(7) and ControlSignals(1) and ALUFunction(2) and ALUFunction(1) and ALUFunction(0);
     -- MemWrite<=ControllerSignal(4);
     -- MemRead<=ControllerSignal(3);
     -- RegWrite<=ControllerSignal(2);
@@ -58,15 +59,19 @@ begin
     ALUComp: ALU port map(Rs, ALUB, ALUFlags, ALUFunction, ControlSignals(9), ALUResult, OutputBeforeMux);
     CCRComp: CCR port map(ALUFlags, ControlSignals(5), BranchFlag);
 
-    OutputPort <= Rs when ControlSignals(2) = '1';
+    OutputPort <= Rs when ControlSignals(2) = '1'
+                 else (others => '0') when rst='1';
 
     STCorCLC <= (not (ControlSignals(8) or ControlSignals(7) or ControlSignals(6) or ControlSignals(4)) or ControlSignals(2)) and (ALUFunction(1) or ALUFunction(0));
     MuxSelector <= RTIBit & STCorCLC when RTIBit = '1'
                     else '0' & STCorCLC;
     
     with MuxSelector select
-        ALUFlags <= OutputBeforeMux when "00",
+        ALUFlagsTemp <= OutputBeforeMux when "00",
                     (ALUFunction(0) & ALUFlags(1 downto 0)) when "01",
                     CCRFromRTI when others;
+    
+    ALUFlags <= (others => '0') when rst='1'
+                else ALUFlagsTemp;
     
 end architecture Exec;
